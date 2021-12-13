5c370f157b5adb49854f0b2965727eb1da95d540
Daniel <dkilcullen25@gmail.com>
2021-12-13 17:02:32 -0600

    commit tree 4e8566e6d94699ea3581348228788830720de239

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let response;
+  if (feet > 2500) {
+    //not allowed
+    response = 'No can do.'
+  } else if (feet > 2000) {
+    //$30
+    response = 'I will gladly take your thirty bucks.';
+  } else {
+    //free sample
+    response = 'This one is on me!';
+  }
+  return response;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return ( city === 'NYC' ? 'Ok, sounds good.' : 'No go.' )
+  
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
